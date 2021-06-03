d1f898ec2c5a2f9b7b6b502796b9dda8d632a054
user_os <user_os>
2021-06-03 12:14:10 +0200

    Queue list based in nodes

--- question_1.py
+++ question_1.py
@@ -1,10 +1,10 @@
 """
-Implement a class FIFO (First in First Out). It should be a class with the following methods:
-    1. Implement a class FIFO (First in First Out). It should be a class with the following methods:
+    Implement a class FIFO (First in First Out).
+    It should be a class with the following methods:
+    1. push: add an element to the class.
     2. pop: get and remove the oldest element.
     3. size: get the number of elements
     4. addition_time: get the datetime when any element present on the FIFO was added.
-        If no position is specified by default return last element addition.
 """
 from datetime import datetime
 import pytz
@@ -15,26 +15,54 @@ class Node(object):
     A class represent node,
     The times are worked in utc-0 and are transformed into local time
     ...
-
-    data : str
-        Any data (default None)
-    addition_time : datetime
-        Creation (default  datetime utc-0)
+    Parameters:
+        data : str
+            Any data (default None)
+        addition_time : datetime
+            Creation (default  datetime utc-0)
+        next:
+            next node (default None)
     """
 
-    def __init__(self, data=None, addition_time=datetime.now(tz=pytz.utc)):
+    def __init__(self, data=None, addition_time=datetime.now(tz=pytz.utc), next=None):
         self.data = data
         self.addition_time = addition_time
+        self.next = next
 
 
 class Queue(object):
+    """
+    Queue list based in nodes
+    ...
+    Parameters:
+        head: First item in the queue
+        tail: Last item in queue
+        size: Tamaño de la cola
+
+    """
+
     def __init__(self):
         self.head = None
         self.tail = None
         self.size = 0
 
     def push(self, data):
-        pass
+        node = Node(data)
+        if self.head is None:
+            self.head = node
+            self.tail = self.head
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.size += 1
 
     def pop(self):
-        pass
+        current = self.head
+        if self.size == 1:
+            self.size -= 1
+            self.head = None
+            self.tail = None
+        elif self.size > 1:
+            self.head = self.head.next
+            self.size -= 1
+        return current
